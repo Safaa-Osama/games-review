5052748db31786540282d70799588e34d99592a3
safaa-osama <safaaosama386@gmail.com>
2025-07-01 18:21:33 +0300

    not enough

--- js/games.js
+++ js/games.js
@@ -1,17 +1,44 @@
 
+import { Ui } from "./ui.js"
 
-class Game{
-constructor(img, title, description, category){
-    this.image = img
-    this.title = title;
-    this.description = description;
-    this.category = category;
 
-}
 
-addGame(){
 
-}
+export class Game {
+    constructor() {
+        this.uiObj = new Ui();
+        this.getGame('sailing');
+       document.querySelectorAll('.navbar a').forEach((linkItem){
+    linkItem.addEventListener('click',  {
+        
+
+    });
+})
+    }
+
+
+
+    async getGame(category) {
+        const url = `https://free-to-play-games-database.p.rapidapi.com/api/games?category=${category}`;
+        const options = {
+            method: 'GET',
+            headers: {
+                'x-rapidapi-key': 'dc55ed9fbemshca07799d31fc69ep10b984jsnba29287ee61d',
+                'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com'
+            }
+        };
+
+        try {
+            const response = await fetch(url, options);
+            const result = await response.json();
+            console.log(result);
+            this.uiObj.displayAllGames(result);
+
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
 
 }
--- js/ui.js
+++ js/ui.js
@@ -1,27 +1,11 @@
-
-// if (localStorage.getItem("myGames") !== null) {
-//     gamesList = JSON.parse(localStorage.getItem("data"));
-//     displayAllGames(data);
-// }
-
-
-// //............object.........
-// developer: "NEOWIZ"
-// genre: "Shooter"
-// id: 533
-// platform: "PC (Windows)"
-// publisher: "NEOWIZ"
-// short_description: "A.V.A is a free-to-play online first-person shooter with multiple game modes, unique customizations, as well as PvP and PvE gameplay."
-// thumbnail: "https://www.freetogame.com/g/533/thumbnail.jpg"
-// title: "A.V.A Global"
-
-
-function displayAllGames(data) {
-    let games = '';
+export class Ui {
+  displayAllGames(data) {
+    let games = "";
     for (let i = 0; i < data.length; i++) {
-        games += ` <div class="main col-md-4 col-lg-3 col-sm-6">
-          <div class="card p-3 bg-black text-light">
-            <img
+      games += ` <div class="main col-md-4 col-lg-3 col-sm-6">
+          <div class="card bg-black text-light h-100">
+<div class="card-body">
+ <img
               src="${data[i].thumbnail}"
               alt="game's picture"
               class="img-thumbnail"
@@ -30,46 +14,40 @@ function displayAllGames(data) {
               <h5>${data[i].title}</h5>
               <button class="btn btn_custom px-1 py-0">Free</button>
             </div>
-            <p>${data[i].description}</p>
+            <p>${data[i].short_description}</p>
+</div>
 
-            <div class="link_group d-flex justify-content-between">
-              <a href="#">${data[i].category}</a>
+           
+
+<div class="link_group d-flex justify-content-between card-footer ">
+              <a href="#">${data[i].genre}</a>
               <a href="#">${data[i].platform}</a>
             </div>
+
           </div>
-        </div>`
+          
+            
+        </div>`;
     }
-    document.getElementById('rowData').innerHTML = games;
-    // localStorage.setItem("myGames", JSON.stringify(data));
+    document.getElementById("rowData").innerHTML = games;
+  }
 
-}
-
-function displayGameDetails(){
- let details = ` <div class="col  w-29">
-            <h2 class="my-2">name of game</h2>
-            <img class="details-img w-100 img-thumbnail" src="${thumbnail}" alt="">
+  displayGameDetails(gameObj) {
+    let details = ` <div class="col-4">
+            <h2 class="my-2">Details Game</h2>
+            <img class="details-img w-100 img-thumbnail" src="${gameObj.thumbnail}" alt="">
         </div>
-        <div class="col p-3 w-69">
-            <h3 class="my-4">title: ${Game.title}</h3>
+        <div class="p-3 col-8">
+            <h3 class="my-4">title: ${gameObj.title}</h3>
 
-            <h5>Category: ${Game.category}<span class="badge text-bg-info>"> mmors</span></h5>
-            <h5>Platform: Lorem, ipsum<span class="badge text-bg-info>"> windows</span></h5>
-            <h5>Status: Lorem, ipsum <span class="badge text-bg-info>"> live</span ></h5>
-            <p class="my-2">${Game.short_description}</p>
+            <h5>Category:<span class="badge text-bg-info"> ${gameObj.genre}</span></h5>
+            <h5>Platform: <span class="badge text-bg-info"> ${gameObj.platform}</span></h5>
+            <h5>Status: ${gameObj.status} <span class="badge text-bg-info">live</span ></h5>
+            <p class="my-2">${gameObj.description}</p>
 
             <button class="btn btn-outline-warning my-3">show games</button>
-        </div>`
-    
+        </div>`;
 
-    document.getElementById('gameDetails').innerHTML = details;
+    document.getElementById("gameDetails").innerHTML = details;
+  }
 }
-
-   
-
-
-
-
-let gameBtn = document.getElementById("gameBtn");
-gameBtn.addEventListener("click", function () {
-  displayGameDetails();
-});
